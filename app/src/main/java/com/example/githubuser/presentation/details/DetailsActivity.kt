8705155ddc900f565036ff58ddc44778ec9acab6
Raka Permana <rakapermanaputraa@gmail.com>
2022-03-18 17:58:01 +0700

    Feat: added details page and fix gradle

---
+++ app/src/main/java/com/example/githubuser/presentation/details/DetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.githubuser.presentation.details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.githubuser.R
+
+class DetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
